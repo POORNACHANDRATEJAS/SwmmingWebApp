a100211aa978a68d36eb071c34611f8252fc1440
callmeankush <ankush.1234singh@gmail.com>
2018-08-19 10:39:10 +0530

    Added trainer photos

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   borImg = 'http://dolphinaquatics.in/images/dolphin_startingblock.jpeg';
-  unimg = 'https://dyn.web.whatsapp.com/pp?e=https%3A%2F%2Fpps.whatsapp.net%2Fv%2Ft61.11540-24%2F37187370_1085749951581270_3221344279385341952_n.jpg%3Foe%3D5B7589FC%26oh%3D2eb7f378b8e913176401e0312adb8ee2&t=l&u=918553423217%40c.us&i=1533452981';
-  sir = 'https://dyn.web.whatsapp.com/pp?e=https%3A%2F%2Fpps.whatsapp.net%2Fv%2Ft61.11540-24%2F37617312_2234287326849532_2940996010035380224_n.jpg%3Foe%3D5B758C32%26oh%3De0e5d0b7bf43367b584958afa65bbec3&t=l&u=918867315438%40c.us&i=1533988086';
+  unimg = 'assets/poorna.jpg';
+  sir = 'assets/ankan.jpg';
   constructor() { }
 
   ngOnInit() {
