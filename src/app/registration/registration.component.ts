c31019348aee73c785294247098796e9d3d4db81
Poorna Chandra Tejas.J <poornachandratejas.j@Poornas-MacBook-Air.local>
2018-08-19 12:15:40 +0530

    modified registration component

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -6,18 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-<<<<<<< HEAD
   personal: { initial: string, firstname: string, lastname: string, middlename: string, gender: string, dob: any, type: string} = {initial: "Select Initial",firstname: "",middlename: "",lastname: "",gender: "",dob: "",type: "PERSONAL_INFO"};
-=======
-  personal: { initial: string, firstname: string } = {initial: "Select Initial",firstname: ""};
-  contact: { mobilenumber: any, emailid: any, pincode: any, city: string, state: any, address: any} = {mobilenumber:"",emailid:"", pincode:"", city:"" , state:"", address:"", type: "conatctinfo"}
->>>>>>> 4a50299b6e9f484470ed2c2923c82ce6d8bd4495
-
+  
+  contact:{ mobilenumber: any, emailid: any, pincode: any,city: string,state: any,address: any, type:string} = {mobilenumber:"",emailid:"",pincode:"",city:"",state:"",address:"",type: "CONTACT_INFO"};
+  
   constructor() { }
   ngOnInit() {
   }
-  saveContactInfo(){
+  saveContactInfo() {
     console.log(this.contact);
+    alert('hi');
   }
   savePersonalInfo() {
   	console.log(this.personal);
