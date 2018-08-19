f5ea9c3360fd04d726f91db2423952ac617c136e
nandinirgowda <nandini.nandu95@gmail.com>
2018-08-19 11:46:40 +0530

    contactinfo

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
   personal: { initial: string, firstname: string } = {initial: "Select Initial",firstname: ""};
+  contact: { mobilenumber: any, emailid: any, pincode: any, city: string, state: any, address: any} = {mobilenumber:"",emailid:"", pincode:"", city:"" , state:"", address:"", type: "conatctinfo"}
 
   constructor() { }
   ngOnInit() {
+  }
+  saveContactInfo(){
+    console.log(this.contact);
   }
   savePersonalInfo() {
   	console.log(this.personal);
