10a8467a7d99421035a040f9a2ab4810e73f500e
Poorna Chandra Tejas.J <poornachandratejas.j@Poornas-MacBook-Air.local>
2018-08-19 11:59:12 +0530

    modified registration component

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  personal: { initial: string, firstname: string, lastname: string, middlename: string, gender: string, dob: any} = {initial: "Select Initial",firstname: "",middlename: "",lastname: "",gender: "",dob: ""};
+  personal: { initial: string, firstname: string, lastname: string, middlename: string, gender: string, dob: any, type: string} = {initial: "Select Initial",firstname: "",middlename: "",lastname: "",gender: "",dob: "",type: "PERSONAL_INFO"};
 
   constructor() { }
   ngOnInit() {
