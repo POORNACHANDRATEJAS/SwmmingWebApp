6d3f1f17daad47e09aad7a1f2ca0495c3bd2e35f
ankantechninzaz <techninzazankan@gmail.com>
2018-08-24 23:53:25 -0700

    contact info saved logic added

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -9,7 +9,7 @@ import { HttpClient } from '@angular/common/http';
 export class RegistrationComponent implements OnInit {
   personal: { initial: string, firstname: string, lastname: string, middlename: string, gender: string, dob: any, type: string} = {initial: "Select Initial",firstname: "",middlename: "",lastname: "",gender: "",dob: "",type: "PERSONAL_INFO"};
   
-  contact:{ mobilenumber: any, emailid: any, pincode: any,city: string,state: any,address: any, type:string} = {mobilenumber:"",emailid:"",pincode:"",city:"",state:"",address:"",type: "CONTACT_INFO"};
+  contact:{ mobilenumber: any, emailid: any, pincode: any,city: string,state: any,address: any, type:string, userid: any } = {mobilenumber:"",emailid:"",pincode:"",city:"",state:"",address:"",type: "CONTACT_INFO", userid: sessionStorage.getItem('userid')};
   
   constructor( public _http: HttpClient) { }
   ngOnInit() {
@@ -17,12 +17,16 @@ export class RegistrationComponent implements OnInit {
   saveContactInfo() {
     console.log(this.contact);
     //alert('hi');
-    
+    this._http.post('http://192.168.1.4:7999/api/v1/dummyApiContact',this.contact).subscribe((data) => {
+      console.log(data);
+      //sessionStorage.setItem('userid',data.result);
+    });
   }
   savePersonalInfo() {
     console.log(this.personal);
     this._http.post('http://192.168.1.4:7999/api/v1/dummyApi',this.personal).subscribe((data) => {
-      console.log(data);
+      //console.log(data);
+      //sessionStorage.setItem('userid',data.result);
     });
   }
 
