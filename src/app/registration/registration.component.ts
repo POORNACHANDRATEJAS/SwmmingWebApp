dcfb33e6ae40a1a213321c93ace937ff1f6a3753
Poorna Chandra Tejas.J <poornachandratejas.j@Poornas-MacBook-Air.local>
2018-08-30 13:48:57 +0530

    modified registration compot

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class RegistrationComponent implements OnInit {
   personal: { initial: string, firstname: string, lastname: string, middlename: string, gender: string, dob: any, type: string} = {initial: "Select Initial",firstname: "",middlename: "",lastname: "",gender: "",dob: "",type: "PERSONAL_INFO"};
   
-  contact:{ mobilenumber: any, emailid: any, pincode: any,city: string,state: any,address: any, type:string} = {mobilenumber:"",emailid:"",pincode:"",city:"",state:"",address:"",type: "CONTACT_INFO"};
+  contact: {  mobilenumber: any, emailid: any, pincode: any,city: string,state: any,address: any, type:string} = {mobilenumber:"",emailid:"",pincode:"",city:"",state:"",address:"",type: "CONTACT_INFO"};
   
   constructor() { }
   ngOnInit() {
